484a1cb85136b39fefb9be9a3bea801c84ec02e6
danieldeng2 <danieldeng223@gmail.com>
2020-11-21 01:19:23 +0000

    Add js files

---
+++ PerlegoContent.js
@@ -0,0 +1,28 @@
+function printMultipleDiv(start, end) {
+    document.querySelector("style").remove();
+    let printContents = "";
+    let i = start;
+    function myLoop() {
+        setTimeout(function() {
+            let content = document.querySelector(`div[data-chapterid='${i}']`);
+            content.scrollIntoView();
+            if (content.querySelector(".pdfplaceholder") == null) {
+                printContents += content.innerHTML + "<br>";
+                i++;
+            }
+            if (i <= end) {
+                myLoop();
+            }
+            if (i == end + 1) {
+                document.body.innerHTML = printContents;
+                console.log("Reading...");
+                setTimeout(()=>{
+                    window.print();
+                    location.reload();
+                }
+                , (end - start) * 100);
+            }
+        }, 1000);
+    }
+    myLoop();
+}
---
+++ PerlegoOutline.js
@@ -0,0 +1,27 @@
+function download(filename, text) {
+    const pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        const event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+const table = Array.from(document.querySelectorAll('h2')).find(el=>el.textContent === 'Table of contents').parentNode;
+const labels = table.querySelectorAll("a");
+let output = "";
+labels.forEach((label)=>{
+    const linkElems = label.getAttribute("href").split('/');
+    const pageNum = linkElems[linkElems.length - 1];
+    const textContent = label.innerText;
+    output += `0 ${pageNum} ${textContent}\n`;
+}
+);
+
+download('outline.txt', output);
